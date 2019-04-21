2cd1d3dac1ccc482f9b0b622d1959501815adbe3
BodneyC <benjcarrington@gmail.com>
2019-04-21 19:17:27 +0100

    Bug fixes

--- scripts/inst-zsh.sh
+++ scripts/inst-zsh.sh
@@ -2,29 +2,28 @@
 
 cp ../zsh/
 
-pacman -S zsh zsh-completions
+sudo pacman -S --noconfirm zsh zsh-completions exa ranger ripgrep fd
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 cp -t ~/ .zshrc .zprofile 
 cp benjc_custom.zsh-theme ~/.oh-my-zsh/custom/
-cp vi-mode-plugin.zsh ~/.oh-my-zsh/custom/plugins
+cp vi-mode-plugin.zsh ~/.oh-my-zsh/custom/plugins/
 
-while true; 
-do
+while true; do
     read -p "Change shell? [y/n]" yn
     case $yn in
-        [Yy]* ) chsh -s /bin/zsh && sudo chsh -s /bin/zsh; break;;
-        [Nn]* ) echo "Shell not changed"; break;;
-        * ) echo "Answer [y/n] please";;
+      [Yy]* ) chsh -s /bin/zsh && sudo chsh -s /bin/zsh; break;;
+      [Nn]* ) echo "Shell not changed"; break;;
+      * ) echo "Answer [y/n] please";;
     esac
 done
 
 # FONTS
-cd ~/gitclones
+mkdir ~/gitclones && cd ~/gitclones
 git clone https://github.com/powerline/fonts.git --depth 1
 cd fonts
 ./install.sh
 
 cd ~/.oh-my-zsh/custom/plugins/
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-cd -
+cd
