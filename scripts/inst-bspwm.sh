afef6e3053e7a8c09e1a99603c804dd1ad3517ae
BodneyC <benjcarrington@gmail.com>
2019-06-27 13:55:29 +0000

    Polybar update

--- scripts/inst-bspwm.sh
+++ scripts/inst-bspwm.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
 
-cd $(dirname "${BASH_SOURCE[0]}")/.. || exit
+cd "$(dirname "${BASH_SOURCE[0]}")"/.. || exit
 
-sudo pacman -S --noconfirm libxcb xcb-util xcb-util-keysims xcb-util-wm bspwm sxhkd yay
-sudo pacman -S --noconfirm gcc make
-sudo pacman -S --noconfirm compton
+sudo pacman -S --noconfirm yay libxcb xcb-util xcb-util-keysims \
+	xcb-util-wm bspwm sxhkd yay xorg-{xbacklight,xev,xsetroot} \
+	gcc make compton
 yay -S --noconfirm xdo sutils xtitle polybar
 
-[[ -d ~/.config ]] || mkdir ~/.config
+[[ -d "$HOME/.config" ]] || mkdir "$HOME/.config"
 
-cp -r -t ~/.config .config/{bspwm,compton.conf,panel,sxhkd,termite}
+cp -r .config/{bspwm,compton.conf,panel,sxhkd,termite} "$HOME/.config"
 
-if [[ -f ~/.xinitrc ]]; then
-	read -p "~/.xinitrc exists, overwrite? [yN] " yn
-
-	case $yn in
-		[yY]*)
-			cp .xinitrc ~
-			;;
-		*)
-			echo "BSPWM not added to ~/.xinitrc"
-			;;
-	esac
-else
-	cp .xinitrc ~
-fi
-
+read -rp -n1 "Copy .xinitrc? [yN] "
+case $REPLY in
+	[yY]*)
+		cp .xinitrc "$HOME"
+		;;
+	*)
+		echo "BSPWM not added to ~/.xinitrc"
+		;;
+esac
--- scripts/inst-polybar.sh
+++ scripts/inst-polybar.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
 TMP_DIR=$(mktemp -d)
-cd $TMP_DIR
+cd "$TMP_DIR" || exit
 
 wget https://use.fontawesome.com/releases/v5.7.2/fontawesome-free-5.7.2-desktop.zip
 unzip fontawesome-free-5.7.2-desktop.zip
 
-cd fontawesome-free-5.7.2-desktop/otfs
-mkdir -p $HOME/.local/share/fonts/
-sudo cp * $HOME/.local/share/fonts/
+mkdir -p "$HOME/.local/share/fonts/"
+sudo cp fontawesome-free-5.7.2-desktop/otfs/* "$HOME/.local/share/fonts/"
 
 fc-cache -fv
 
-cd $TMP_DIR
-git clone https://aur.archlinux.org/polybar.git
-cd polybar
-makepkg -s
+hash yay || sudo pacman -S yay
 
-# Vague due to version numbers
-sudo pacman -U $(ls -A1 | grep pkg.tar.xz) --noconfirm
+cp -r "$(dirname "${BASH_SOURCE[0]}")"/../.config/polybar "$HOME/.config"
