2cd6a5eed6a5de10dd7c278e505cfa33862431ef
BodneyC <benjcarrington@gmail.com>
2020-01-26 19:32:16 +0000

    Setup for second monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,7 +1,12 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar main -c ~/.config/polybar/config.ini &
+N_MONITORS="$(xrandr -q | rg -c ' connected')"
+CONFIG_FILE="$HOME/.config/polybar/config.ini"
+[ "$N_MONITORS" -gt 1 ] && CONFIG_FILE="$HOME/.config/polybar/config-2.ini"
+echo "$CONFIG_FILE"
+
+polybar main -c "$CONFIG_FILE" &
