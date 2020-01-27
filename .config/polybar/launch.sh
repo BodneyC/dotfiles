6cd8ed410422cbda4dde1bb80961b5c3cad0f603
BodneyC <benjcarrington@gmail.com>
2020-01-27 19:20:47 +0000

    Polybar changes and some dots

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,8 +5,10 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 N_MONITORS="$(xrandr -q | rg -c ' connected')"
-CONFIG_FILE="$HOME/.config/polybar/config.ini"
-[ "$N_MONITORS" -gt 1 ] && CONFIG_FILE="$HOME/.config/polybar/config-2.ini"
-echo "$CONFIG_FILE"
 
-polybar main -c "$CONFIG_FILE" &
+if [ "$N_MONITORS" -gt 1 ]; then
+	polybar main -c "$HOME/.config/polybar/config-DisplayPort-0.ini" &
+	polybar main -c "$HOME/.config/polybar/config-DisplayPort-1.ini" &
+else
+	polybar main -c "$HOME/.config/polybar/config.ini" &
+fi
