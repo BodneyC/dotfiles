116f88487f5406016fcacbcdcd695b69b9bfc613
BodneyC <benjcarrington@gmail.com>
2020-12-09 18:20:26 +0000

    feat(switching): Better alt+tab switching in i3

--- .config/i3/cycle.sh
+++ .config/i3/cycle.sh
@@ -3,7 +3,29 @@
 command -v jq &> /dev/null || return 1
 command -v i3-msg &> /dev/null || return 1
 
-export _cycle_dir="${1:-up}"
+if [[ -z "$1" ]]; then
+  echo "No command specified"
+  exit 1
+fi
+
+_cycle_dir="$1"
+
+TOGGLE_FILE="/tmp/i3.toggle.indicator"
+if [[ "$1" =~ "toggle" ]]; then
+  if [[ ! -f "$TOGGLE_FILE" ]]; then
+    echo "down" > "$TOGGLE_FILE"
+  fi
+  _cycle_dir="$(<"$TOGGLE_FILE")"
+fi
+
+if [[ "$_cycle_dir" == "up" ]]; then
+  _toggle="down"
+else
+  _toggle="up"
+fi
+echo "$_toggle" > "$TOGGLE_FILE"
+
+export _cycle_dir
 
 # I'm strangely proud of this...
 _i3_focused="$(i3-msg -t get_tree | jq '
